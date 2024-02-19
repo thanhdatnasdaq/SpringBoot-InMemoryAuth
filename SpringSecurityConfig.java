6c1fdb336b4d54bb72da880258c2295a0a7e0d0f
thanhdatnasdaq <122451214+thanhdatnasdaq@users.noreply.github.com>
2024-02-19 21:20:29 +1100

    Create SpringSecurityConfig.java

---
+++ SpringSecurityConfig.java
@@ -0,0 +1,43 @@
+package leotran.demo.TikTok.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SpringSecurityConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder(){ return new BCryptPasswordEncoder(); }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) ->{
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.builder()
+                .username("tiktok_user1")
+                .password(passwordEncoder().encode("ilovetiktok"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+}
